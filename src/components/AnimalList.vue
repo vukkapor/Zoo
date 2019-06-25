97c51d84a67d9f041df1413d0d7786f6ac1aae00
Vuk Kapor <vukkapor12@gmail.com>
2019-06-25 18:53:51 +0200

    added delete

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -9,8 +9,8 @@
       <tr v-for="(animal, index) in animals" :key="index">
         <td>{{animal.species}}</td>
         <td>{{animal.name}}</td>
-        <td v-if="animal.dateOfBirth!=null">{{animal.dateOfBirth}}</td>
-        <td v-else>Nepoznat</td>
+        <td >{{animal.dateOfBirth || "Nepoznat"}}</td>
+        <td> <button type="submit" @click="deleteAnimal(index)">Delete</button> </td>
       </tr>
     </table>
   </div>
@@ -22,12 +22,18 @@ export default {
     return {
       animals: [
         { species: "Wolf1", name: "Wolf1", dateOfBirth: "12.01" },
-        { species: "Wolf2", name: "Wolf2", dateOfBirth: null },
+        { species: "Wolf2", name: "Wolf2", dateOfBirth: NaN },
         { species: "Wolf3", name: "Wolf3", dateOfBirth: "12.01" },
         { species: "Wolf4", name: "Wolf4", dateOfBirth: "12.01" },
         { species: "Wolf5", name: "Wolf5", dateOfBirth: "12.01" }
       ]
     };
+  },
+
+  methods: {
+    deleteAnimal(index) {
+      this.animals.splice(index, 1);
+    }
   }
 };
 </script>
