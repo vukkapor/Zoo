3042f5a8c0a8021ba0f05262cd766d4cfc2b36bf
Vuk Kapor <vukkapor12@gmail.com>
2019-06-25 19:38:58 +0200

    Animal type

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -13,18 +13,29 @@
         <label for="dateOfBirth">Date of birth</label>
         <input type="text" id="dateOfBirth" v-model="newAnimal.dateOfBirth">
       </div>
-      <div><button type="submit">Add a new animal</button></div>
+      <div>
+        <select v-model="newAnimal.animalType">
+          <option disabled value>Please select one</option>
+          <option v-for="(type, index) in animalTypes" :key="index">{{type}} </option>
+        </select>
+        <span>Selected: {{ selected }}</span>
+      </div>
+      <div>
+        <button type="submit">Add a new animal</button>
+      </div>
     </form>
     <table style="width:100%">
       <tr>
         <th>Species</th>
         <th>Name</th>
         <th>Date of birth</th>
+        <th>Animal Type</th>
       </tr>
       <tr v-for="(animal, index) in animals" :key="index">
         <td>{{animal.species}}</td>
         <td>{{animal.name}}</td>
         <td>{{animal.dateOfBirth || "Nepoznat"}}</td>
+        <td>{{animal.animalType}}</td>
         <td>
           <button type="submit" @click="deleteAnimal(index)">Delete</button>
         </td>
@@ -43,12 +54,14 @@ export default {
       newAnimal: this.defaultAnimal(),
 
       animals: [
-        { species: "Wolf1", name: "Wolf1", dateOfBirth: "12.01" },
-        { species: "Wolf2", name: "Wolf2", dateOfBirth: NaN },
-        { species: "Wolf3", name: "Wolf3", dateOfBirth: "12.01" },
-        { species: "Wolf4", name: "Wolf4", dateOfBirth: "12.01" },
-        { species: "Wolf5", name: "Wolf5", dateOfBirth: "12.01" }
-      ]
+        { species: "Wolf1", name: "Wolf1", dateOfBirth: "12.01", animalType: "nesto" },
+        { species: "Wolf2", name: "Wolf2", dateOfBirth: NaN, animalType: "nesto"  },
+        { species: "Wolf3", name: "Wolf3", dateOfBirth: "12.01", animalType: "nesto"  },
+        { species: "Wolf4", name: "Wolf4", dateOfBirth: "12.01", animalType: "nesto"  },
+        { species: "Wolf5", name: "Wolf5", dateOfBirth: "12.01", animalType: "nesto"  }
+      ],
+
+      animalTypes: ['ptice', 'sisari', 'svasta']
     };
   },
 
@@ -56,9 +69,10 @@ export default {
     defaultAnimal() {
       return {
         species: "",
-        name: "", 
-        dateOfBirth: ""
-      }
+        name: "",
+        dateOfBirth: "",
+        animalType: "unknown"
+      };
     },
     deleteAnimal(index) {
       this.animals.splice(index, 1);
@@ -70,8 +84,8 @@ export default {
     },
 
     addAnimal() {
-      this.animals.push({...this.newAnimal})
-      this.newAnimal=this.defaultAnimal();
+      this.animals.push({ ...this.newAnimal });
+      this.newAnimal = this.defaultAnimal();
     }
   }
 };
